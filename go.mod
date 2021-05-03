60188c8d546242a3a4c3916084f3c82311aaf9f1
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-05-03 17:42:59 +0900

    Add Webhook's struct

---
+++ go.mod
@@ -0,0 +1,5 @@
+module shopifyCreateUser
+
+go 1.16
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
     "time"
 
 		"github.com/joho/godotenv"
-    goshopify "github.com/tao-s/go-shopify"
+    goshopify "github.com/bold-commerce/go-shopify"
 )
 
 // Create an app somewhere.
@@ -20,4 +20,20 @@ app := goshopify.App{
 	ApiSecret: "efgh",
 	RedirectUrl: "https://example.com/shopify/callback",
 	Scope: "read_products,read_orders",
+}
+
+b, err := ioutil.ReadAll(r.Body)//リクエストのbodyをパース
+defer r.Body.Close()
+if err != nil {
+    panic(err)
+}
+var jsonData Webhook//jsonの形式に合わせたstructを初期化
+if err := json.Unmarshal(b, &jsonData); err != nil {//jsonをパースして構造体に値を入れる。
+    panic(err)
+}
+//Webgook Webhookの構造体
+type Webhook = struct {
+    ID              json.Number `json:id`//これがID。受注番号とは違う。
+    Gateway         string      `json:gateway`
+    FinancialStatus string      `json:financial_status`//支払いステータス 読み取り専用
 }
